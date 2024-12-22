76464b06e2c43fe6ac020d4367e616e8b87d3125
dinakomariah584 <dinakomariah584@gmail.com>
2024-12-22 10:06:10 +0700

    tugas13

--- toko-buku-online/app/Database/Migrations/2024-12-12-033401_CreatePelangganTable.php
+++ toko-buku-online/app/Database/Migrations/2024-12-12-033401_CreatePelangganTable.php
@@ -9,35 +9,38 @@ class CreatePelangganTable extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id_pelanggan'=>[
-                'type'           => 'INT',
-                'constraint'     => 11,
-                'unsigned'       => true,
-                'auto_increment' => true,
-            ],
-            'id_user'=>[
-                'type'           => 'INT',
-                'constraint'     => 11,
-                'unsigned'       => true,
-            ],
-            'nama'=>[
-                'type'           => 'VARCHAR',
-                'constraint'     => 255,
-            ],
-            'no_hp'=>[
-                'type'           => 'VARCHAR',
-                'constraint'     => 16,
-            ],
-            'alamat'=>[
-                'type'           => 'VARCHAR',
-                'constraint'     => 255
-            ],
+            'id_pelanggan' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => TRUE,
+                'auto_increment' => TRUE
 
-        ]);
+            ],
+            'id_user' => [
+                'type'          => 'INT',
+                'constraint'    => 11,
+                'unsigned'      => TRUE,
+            ],
+            'nama' => [
+                'type'          => 'VARCHAR',
+                'constraint'    => 225,
+            ],
+            'no_hp' => [
+                'type'          => 'VARCHAR',
+                'constraint'    => 16,
+            ],
+            'alamat' => [
+                'type'          => 'VARCHAR',
+                'constraint'    => 225,
+            ]
 
-        $this->forge->addKey('id_pelanggan', true);
-        $this->forge->addForeignKey('id_user', 'users', 'id');
-        $this->forge->createTable('customers');
+
+
+            ]);
+
+            $this->forge->addKey('id_pelanggan', true);
+            $this->forge->addForeignKey('id_user', 'users', 'id');
+            $this->forge->createTable('customers');
     }
 
     public function down()
--- toko-buku-online/app/Views/admin/databuku.php
+++ toko-buku-online/app/Views/admin/databuku.php
@@ -1,202 +1,168 @@
 <?= $this->extend('admin/layout'); ?>
 
-<?= $this->section('content'); ?>
-
-<div class="col-9">
-    <br>
-    <h1>Data Buku</h1>
-    <!-- Topbar -->
-
-    <!-- Sidebar Toggle (Topbar) -->
-    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-        <i class="fa fa-bars"></i>
-    </button>
-
-    <!-- Topbar Search -->
-    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-        <div class="input-group">
-            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                aria-label="Search" aria-describedby="basic-addon2">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                    <i class="fas fa-search fa-sm"></i>
-                </button>
+<?= $this->section('mainadmin'); ?>
+<div class="px-4">
+    <div class="d-flex justify-content-between align-items-center mb-3">
+        <h4 class="mb-0">Data Buku</h4>
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambahModal">
+            <i class="bi bi-plus-circle me-2"></i>Tambah Buku
+        </button>
+        <?php if (session('sukses')): ?>
+            <div class="mb-3">
+                <div class="alert alert-success">
+                    <strong>Sukses</strong> <?= session('sukses') ?>
+                </div>
             </div>
-        </div>
-    </form>
+        <?php endif ?>
 
-    <hr>
-    <div class="my-3">
-        <button data-bs-toggle="modal" data-bs-target="#tambahModal" class="btn btn-dark"><i class="bi bi-plus-lg"></i>+
-            Tambah Data</button>
+        <?php if (session('gagal')): ?>
+            <div class="mb-3">
+                <div class="alert alert-danger">
+                    <strong>Gagal</strong> <?= session('gagal') ?>
+                </div>
+            </div>
+        <?php endif ?>
     </div>
 
-    <table class="table table-striped mt-3">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Judul</th>
-                <th>Pengarang</th>
-                <th>Penerbit</th>
-                <th>Tahun</th>
-                <th>Thumbnail URL</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
 
-            <!-- Data Dummy -->
-            <tr>
-                <td>1</td>
-                <td>Lord Makan Orang</td>
-                <td>Sigma</td>
-                <td>Gramedia</td>
-                <td>2023</td>
-                <td>https://google.com/1.jpg</td>
-                <td>
-                    <button class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editModal"
-                        data-bs-id="1"><i class="bi bi-pencil-square"></i> Edit</button>
-                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#hapusModal"
-                        data-bs-id="1"><i class="bi bi-trash"></i> Hapus</button>
-                </td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>Aku Dan Icikiwir</td>
-                <td>Asep</td>
-                <td>Gramedia</td>
-                <td>2020</td>
-                <td>https://google.com/2.jpg</td>
-                <td>
-                    <button class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editModal"
-                        data-bs-id="2"><i class="bi bi-pencil-square"></i> Edit</button>
-                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#hapusModal"
-                        data-bs-id="2"><i class="bi bi-trash"></i> Hapus</button>
-                </td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>Cinta Itu Oraora</td>
-                <td>Eka Kurniawan</td>
-                <td>Gramedia</td>
-                <td>2018</td>
-                <td>https://google.com/3.jpg</td>
-                <td>
-                    <button class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#editModal"
-                        data-bs-id="2"><i class="bi bi-pencil-square"></i> Edit</button>
-                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#hapusModal"
-                        data-bs-id="2"><i class="bi bi-trash"></i> Hapus</button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-</div>
-
-<!-- Modal Tambah Buku -->
-<div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title fs-5" id="exampleModalLabel">Form Tambah Buku</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="/databuku.php/simpan" method="POST" id="formTambah">
-                    <div class="mb-3">
-                        <label for="judul">Judul</label>
-                        <input type="text" name="judul" id="judul" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label for="pengarang">Pengarang</label>
-                        <input type="text" name="pengarang" id="pengarang" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label for="penerbit">Penerbit</label>
-                        <input type="text" name="penerbit" id="penerbit" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label for="tahun">Tahun</label>
-                        <input type="text" name="tahun" id="tahun" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label for="tahun">Thumbnail URL</label>
-                        <input type="url" name="thumbnail_url" id="thumbnail_url" class="form-control"
-                            placeholder="https:/example.com/image.jpg">
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" form="formTambah" class="btn btn-primary"><i
-                        class="bi bi-floppy"></i>Simpan</button>
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Judul</th>
+                            <th scope="col">Pengarang</th>
+                            <th scope="col">Penerbit</th>
+                            <th scope="col">Tahun</th>
+                            <th scope="col">Thumbnail URL</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($books as $buku) : ?>
+                            <tr>
+                                <td><?= $buku['id']; ?></td>
+                                <td><?= $buku['judul']; ?></td>
+                                <td><?= $buku['pengarang']; ?></td>
+                                <td><?= $buku['penerbit']; ?></td>
+                                <td><?= $buku['tahun']; ?></td>
+                                <td>
+                                    <img src="<?= base_url($buku['thumbnail_url']) ?>" alt="<?= $buku['judul'] ?>" class="img-thumbnail"
+                                        style="width: 150px; height: auto">
+                                </td>
+                                <td>
+                                    <a href="<?= base_url('admin/databuku/' . $buku['id'] . '/edit')?>" class="btn btn-sm btn-success"><i class="bi bi-pencil-square"></i></a>
+                                    <button class="btn btn-sm btn-danger" data-bs-id="<?= $buku['id']; ?>" data-bs-toggle="modal" data-bs-target="#hapusModal"><i class="bi bi-trash3-fill"></i></button>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-</div>
 
-<!-- Modal hapus-->
-<div class="modal fade" id="hapusModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title fs-5" id="exampleModalLabel">Konfirmasi Hapus Data</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="/index.php/hapus" method="POST" id="formHapus">
-                    <input type="hidden" name="id" id="idHapus" value="">
-                </form>
-                <p class="">Apakah Anda yakin menghapus data dengan id <span id="textId"></span> ini?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" form="formHapus" class="btn btn-danger"><i class="bi-trash"></i>Hapus</button>
+    <!-- Modal Tambah -->
+    <div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title fs-5" id="exampleModalLabel">Form Tambah Buku</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="<?= base_url('admin/databuku') ?>" method="POST" enctype="multipart/form-data"
+                        id="formTambah">
+                        <div class="mb-3">
+                            <label for="judul">Judul</label>
+                            <input type="text" name="judul" id="judul" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="pengarang">Pengarang</label>
+                            <input type="text" name="pengarang" id="pengarang" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="penerbit">Penerbit</label>
+                            <input type="text" name="penerbit" id="penerbit" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="tahun">Tahun</label>
+                            <input type="text" name="tahun" id="tahun" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="tahun">Cover</label>
+                            <input type="file" accept="image/*" name="thumbnail_url" id="thumbnail_url"
+                                class="form-control">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" form="formTambah" class="btn btn-primary">Simpan</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<!-- Modal Edit Buku  -->
-<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title fs-5" id="exampleModalLabel">Form Edit Buku</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <!-- Modal Edit -->
+    <div class="modal fade" id="editModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Edit Buku</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="editBukuForm">
+                        <div class="mb-3">
+                            <label for="judulbuku" class="form-label">Judul Buku</label>
+                            <input type="text" class="form-control" id="judulbuku" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="pengarang" class="form-label">Pengarang</label>
+                            <input type="text" class="form-control" id="pengarang" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="penerbit" class="form-label">Penerbit</label>
+                            <input type="text" class="form-control" id="penerbit" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="tahun" class="form-label">Tahun</label>
+                            <input type="number" class="form-control" id="tahun" min="1900" max="2024" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="thumbnailurl" class="form-label">Thumbnail URL</label>
+                            <input type="url" class="form-control" id="thumbnailurl" required>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                    <button type="submit" form="editBukuForm" class="btn btn-success">Simpan</button>
+                </div>
             </div>
-            <div class="modal-body">
-                <form action="/index.php/edit" method="POST" id="formEdit">
-                    <input type="hidden" name="id" id="idEdit">
-                    <div class="mb-3">
-                        <label for="judulEdit">Judul</label>
-                        <input type="text" name="judul" id="judulEdit" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label for="pengarangEdit">Pengarang</label>
-                        <input type="text" name="pengarang" id="pengarangEdit" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label for="penerbitEdit">Penerbit</label>
-                        <input type="text" name="penerbit" id="penerbitEdit" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label for="tahunEdit">Tahun</label>
-                        <input type="text" name="tahun" id="tahunEdit" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label for="tahunEdit">Thumbnail URL</label>
-                        <input type="url" name="thumbnail" id="thumbnailEdit" class="form-control"
-                            placeholder="https:/example.com/image.jpg">
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="submit" form="formEdit" class="btn btn-primary"><i class="bi bi-save"></i> Simpan</button>
+        </div>
+    </div>
+
+    <!-- Modal Hapus -->
+    <div class="modal fade" id="hapusModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Hapus Buku</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Apakah anda yakin menghapus data ini?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                    <button type="button" class="btn btn-danger">Hapus</button>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
-<?= $this->endSection(); ?>
+<?= $this->endSection() ?>
